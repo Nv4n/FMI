5cb57dad6646adb0711121215f4b4834ea599786
Sybatron <vipsister2014@gmail.com>
2022-03-30 11:02:23 +0300

    Haskell week 03 finished
    +Started Week 04

--- Course1/Haskell/Week03/HaskellWeek03.hs
+++ Course1/Haskell/Week03/HaskellWeek03.hs
@@ -1,12 +1,17 @@
 main :: IO ()
 main = do
-	print (pow 5.5 3)
-	print (isPrime 100)
-	print (isPrime 997)
-	print (isAscending 1234)
-	print (isAscending 1204)
-	print (countOccurences 153161 1)
-	print (countOccurences 0 0)
+	-- print (pow 5.5 3)
+	-- print (isPrime 100)
+	-- print (isPrime 997)
+	-- print (isAscending 1234)
+	-- print (isAscending 1204)
+	-- print (countOccurences 153161 1)
+	-- print (countOccurences 0 0)
+	print (isPerfectNumber 100)
+	print (isPerfectNumber 496)
+	print (sumPrimeDivisors 100)
+	print (sumPrimeDivisors 83)
+
 
 -- Задача 1. Да се дефинира функция pow, която генерира линейно рекурсивен процес и намира x на степен n, където x е реално, а n - естествено число.
 
@@ -49,3 +54,22 @@ countOccurences num numToFind =
 				| num == 0                   = count
 				| num `mod` 10 == numToFind  = helper (count + 1) (num `div` 10)
 				| otherwise                  = helper count (num `div` 10)
+
+-- Да се дефинира предикат isPerfectNumber, който връща дали едно число е съвършено, т.е. равно на сумата от делителите му.
+isPerfectNumber:: Int -> Bool
+isPerfectNumber num = num == helper 0 1
+	where
+	    helper divSum divider
+			| divider == num = divSum
+	    	| num `mod` divider == 0 = helper (divSum + divider) (divider + 1)
+	    	| otherwise = helper divSum (divider + 1)
+
+-- Задача 6. Да се дефинира функция sumPrimeDivisors, която намира сумата на всички прости делители на едно число.
+
+sumPrimeDivisors::Int -> Int 
+sumPrimeDivisors num = helper 0 2
+	where 
+		helper sum divider
+			| divider == num + 1= sum
+			| num `mod` divider == 0 && isPrime divider = helper (sum + divider) (divider+1)
+			| otherwise = helper sum (divider+1)
