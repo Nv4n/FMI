c6a58955fe7fc37447c8b9e96eb70ef4c8ebf496
Sybatron <vipsister2014@gmail.com>
2022-10-14 17:01:48 +0300

    Updated solutions lab2 exercisesB

--- Course2/JavaOOP/Week02/Lab2/src/exercises/b/Zad1/Prime.java
+++ Course2/JavaOOP/Week02/Lab2/src/exercises/b/Zad1/Prime.java
@@ -1,4 +1,60 @@
 package exercises.b.Zad1;
 
 public class Prime {
+    private long sqrtTime;
+    private long dividedBy2Time;
+    private int primeNumber;
+
+    public Prime() {
+        primeNumber = 2;
+        sqrtTime = 0;
+        dividedBy2Time = 0;
+    }
+
+    public int getPrimeNumber() {
+        return primeNumber;
+    }
+
+    public long getSqrtTime() {
+        return sqrtTime;
+    }
+
+    public long getDividedBy2Time() {
+        return dividedBy2Time;
+    }
+
+    public void nextPrime() {
+        for (int i = primeNumber + 1; ; i++) {
+            if (isPrime(i)) {
+                primeNumber = i;
+                return;
+            }
+        }
+    }
+
+    private boolean isPrime(int number) {
+        boolean flag = true;
+        long start = System.nanoTime();
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        long end = System.nanoTime();
+        sqrtTime += (end - start);
+        start = System.nanoTime();
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        end = System.nanoTime();
+        dividedBy2Time += (end - start);
+        return flag;
+    }
+
 }
--- Course2/JavaOOP/Week02/Lab2/src/exercises/b/Zad1/PrimeTest.java
+++ Course2/JavaOOP/Week02/Lab2/src/exercises/b/Zad1/PrimeTest.java
@@ -1,4 +1,19 @@
 package exercises.b.Zad1;
 
 public class PrimeTest {
+    private static final int N_LIMIT = 10_000;
+
+    public static void main(String[] args) {
+        Prime prime = new Prime();
+
+        while (prime.getPrimeNumber() <= N_LIMIT) {
+            System.out.println(prime.getPrimeNumber());
+            prime.nextPrime();
+        }
+
+        System.out.printf("Time with divided by 2 is: %,d nanoseconds%nTime with sqrt is: %,d nanoseconds%n",
+                prime.getDividedBy2Time(),
+                prime.getSqrtTime());
+
+    }
 }
