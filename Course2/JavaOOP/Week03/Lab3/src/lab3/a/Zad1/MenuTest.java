d2c6bfefccc97ec1bc4526e655573d936674513c
Sybatron <vipsister2014@gmail.com>
2022-10-17 18:42:28 +0300

    Updated Java OOP Lab3a solutions

---
+++ Course2/JavaOOP/Week03/Lab3/src/lab3/a/Zad1/MenuTest.java
@@ -0,0 +1,8 @@
+package lab3.a.Zad1;
+
+public class MenuTest {
+    public static void main(String[] args) {
+        ComputeByMenu compMenu = new ComputeByMenu(0.00);
+        compMenu.getUserChoice();
+    }
+}
