30b7d81cd91b71c969f74d0c7d2f8af2bc5c84e4
Sybatron <vipsister2014@gmail.com>
2022-12-15 04:02:10 +0200

    exercise [front-end-web] week11/ live-coding completed

--- Course2/Front-EndWeb/Week11/live-coding/index.js
+++ Course2/Front-EndWeb/Week11/live-coding/index.js
@@ -1,19 +1,19 @@
-const input = document.getElementById('task-input');
-const list = document.getElementById('list');
+const input = document.getElementById("task-input");
+const list = document.getElementById("list");
 
-input.addEventListener('keydown', (event) => {
-    const isEnter = event.key === 'Enter';
-    const value = event.target.value;
+input.addEventListener("keydown", (event) => {
+    const isEnter = event.key === "Enter";
+    let value = event.target.value;
     if (!isEnter || !value) {
         return;
     }
 
-    const task = document.createElement('li');
-    task.className = 'task';
-    const expPattern = /# (\d [*/+-] ){1,}\d/
+    const task = document.createElement("li");
+    task.className = "task";
+    const expPattern = /^# (\d [*/+-] ){1,}\d$/;
     if (expPattern.test(value)) {
-        value = value.replace('#', '').trim();
-        value = `${value} = ${eval(value)}`;
+        value = value.replace("#", "").trim();
+        value = `${value} = ${evalExp(value)}`;
     }
 
     task.innerHTML = `
@@ -23,17 +23,17 @@ input.addEventListener('keydown', (event) => {
 `;
 
     list.appendChild(task);
-    input.value = '';
+    input.value = "";
     updateCounter();
 });
 
-list.addEventListener('change', (event) => {
+list.addEventListener("change", (event) => {
     const task = event.target.parentElement;
-    task.classList.toggle('done', event.target.checked);
+    task.classList.toggle("done", event.target.checked);
     updateCounter();
 });
 
-list.addEventListener('click', (event) => {
+list.addEventListener("click", (event) => {
     const isRemoveBtn = event.target.classList.contains("btn-remove");
     if (!isRemoveBtn) {
         return;
@@ -43,31 +43,57 @@ list.addEventListener('click', (event) => {
     updateCounter();
 });
 
-const filterBtns = document.querySelector('.filter-container');
-filterBtns.addEventListener('click', (event) => {
+const filterBtns = document.querySelector(".filter-container");
+filterBtns.addEventListener("click", (event) => {
     for (const element of filterBtns.children) {
-        element.classList.remove('active');
+        element.classList.remove("active");
     }
     const filter = event.target;
-    filter.classList.toggle('active');
-})
+    filter.classList.toggle("active");
 
-
-const clearBtn = document.getElementById('clear-completed');
-clearBtn.addEventListener('click', () => {
-    Array.from(list.children).forEach((el) => {
-        if (el.classList.contains('done')) {
-            list.removeChild(el);
-        }
-    })
+    switch (filter.id) {
+        case "all-btn":
+            for (const task of list.children) {
+                task.classList.remove("hidden");
+            }
+            break;
+        case "active-btn":
+            for (const task of list.children) {
+                if (task.classList.contains("done")) {
+                    task.classList.add("hidden");
+                } else {
+                    task.classList.remove("hidden");
+                }
+            }
+            break;
+        case "completed-btn":
+            for (const task of list.children) {
+                if (!task.classList.contains("done")) {
+                    task.classList.add("hidden");
+                } else {
+                    task.classList.remove("hidden");
+                }
+            }
+            break;
+        default:
+            break;
+    }
 });
 
+const clearBtn = document.getElementById("clear-completed");
+clearBtn.addEventListener("click", () => {
+    Array.from(list.children).forEach((el) => {
+        if (el.classList.contains("done")) {
+            list.removeChild(el);
+        }
+    });
+});
 
-const countLabel = document.getElementById('tasks-left');
+const countLabel = document.getElementById("tasks-left");
 function updateCounter() {
     let count = 0;
     for (const task of list.children) {
-        if (!task.classList.contains('done')) {
+        if (!task.classList.contains("done")) {
             count++;
         }
     }
@@ -79,16 +105,49 @@ updateCounter();
 
 function evalExp(expression) {
     const nums = [];
-    const operators = [];
-    let res = undefined;
-    expression.forEach((char) => {
-        if (char === ' ') {
-            return;
+    const ops = [];
+    for (const char of expression) {
+        if (char === " ") {
+            continue;
         }
         if (/\d/.test(char)) {
-            nums.push(char.charCodeAt(0) - '0'.charCodeAt(0));
+            nums.push(char.charCodeAt(0) - "0".charCodeAt(0));
         } else if (/[*/+-]/.test(char)) {
-            operators.push(char);
+            ops.push(char);
         }
-    })
+    };
+    //!FIXME Need to make the evaluate accurate
+    function evaluate() {
+        if (nums.length <= 1) {
+            return;
+        }
+        const operand1 = nums.pop();
+        const operand2 = nums.pop();
+        switch (ops.pop()) {
+            case "+":
+                nums.push(operand2 + operand1);
+
+                break;
+            case "-":
+                nums.push(operand2 - operand1);
+                break;
+            case "*":
+                nums.push(operand2 * operand1);
+                break;
+            case "/": {
+                if (operand1 === 0) {
+                    nums.push(NaN);
+                    return;
+                }
+                nums.push(operand2 / operand1);
+                break;
+            }
+            default:
+                console.error('bad stuff happened');
+                break;
+        }
+        evaluate(nums, ops);
+    }
+    evaluate();
+    return nums.pop();
 }
