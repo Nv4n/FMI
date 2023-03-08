82680ec71cbdf586ff4943ed73f21b6e2756e511
Sybatron <vipsister2014@gmail.com>
2023-03-08 12:27:19 +0200

    Added demo/intro into R

---
+++ Course3/Statistics/Week01/demo.R
@@ -0,0 +1,45 @@
+
+
+#?mean
+#help("sd");
+
+#??liniear
+#help.search("anova")
+#??"liniear model"
+
+# x=5
+# y<-2*x+6
+# "Austria"->z
+# x; y; z;
+# 
+v1<-c()
+v1<-c(1,4,6.,4,7,12.,-17,1)
+v1
+# 
+# v2<-c("a","b","c")
+# v2
+# 
+# v12<-c(v1,v2)
+# v12
+# 
+# 
+# str(v1)
+# class(v1)
+# 
+# str(v2)
+# class(v2)
+# 
+# str(v12)
+
+v3 <-as.Date(c("2015-01-01","2016-07-08"))
+v3
+
+v4<-as.Date(c("01.02.2015","08.07.2016"),format="%m/%d/%y",origin=1900)
+v4
+
+v1[1]
+v1[c(1,length(v1))]
+v1[1:length(v1)]
+
+seq(from=1, to=10,by=2)
+
