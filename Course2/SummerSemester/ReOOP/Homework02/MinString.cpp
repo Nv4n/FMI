8b2acf7a423eede802238a8bcb2aba9e931e409c
Sybatron <vipsister2014@gmail.com>
2023-05-26 22:01:55 +0300

    MinString finished

--- Course2/SummerSemester/ReOOP/Homework02/MinString.cpp
+++ Course2/SummerSemester/ReOOP/Homework02/MinString.cpp
@@ -13,36 +13,101 @@ MinString::MinString() {
 
 }
 
+MinString::~MinString() {
+    destroy();
+}
+
+// Copy
 MinString::MinString(const char *_data) {
-    length = strlen(_data);
-    size = length + 1;
-    data = new char[size];
-    std::strcpy(data, _data);
+    copy(_data);
 }
 
-bool operator==(const MinString &lhs, const MinString &rhs) {
-    return std::strcmp(lhs.data, rhs.data) == 0;
+MinString::MinString(const MinString &other) {
+    copy(other);
 }
 
+MinString &MinString::operator=(const MinString &other) {
+    if (this != &other) {
+        destroy();
+        copy(other);
+    }
+    return *this;
+}
+
+MinString &MinString::operator=(const char *_data) {
+    if (std::strcmp(data, _data) != 0) {
+        destroy();
+        copy(_data);
+    }
+    return *this;
+}
+
+// Move
+MinString::MinString(MinString &&other) noexcept {
+    move(other);
+}
+
+MinString &MinString::operator=(MinString &&other) noexcept {
+    if (this != &other) {
+        destroy();
+        move(other);
+    }
+    return *this;
+}
+
+// Getter and setters
+size_t MinString::getLength() const {
+    return length;
+}
+
+// Operators
+MinString &operator+(const MinString &lvs, const MinString &rvs) {
+    MinString result;
+    result += lvs;
+    result += rvs;
+    return result;
+}
+
+MinString &MinString::operator+=(const MinString &other) {
+    if (length + other.length + 1 < size) {
+        scaleUp();
+    }
+    char *temp = new char[length + other.length + 1];
+    std::strcpy(temp, data);
+    std::strcat(temp, other.data);
+
+    delete[] data;
+    length += other.length;
+    data = temp;
+
+    return *this;
+}
+
+// Comparators
+bool operator==(const MinString &lvs, const MinString &rvs) {
+    return std::strcmp(lvs.data, rvs.data) == 0;
+}
+
+bool operator!=(const MinString &lvs, const MinString &rvs) {
+    return !(lvs == rvs);
+}
+
+// Read and write
 std::istream &operator>>(std::istream &is, MinString &str) {
     const int bufferSize = 1024;
     char buffer[bufferSize];
     int i = 0;
 
     while (i < bufferSize - 1) {
-        int ch = is.get();
+        char ch;
+        is.get(ch);
         if (ch == '\n' || ch == '\0')
             break;
         buffer[i++] = ch;
     }
 
     buffer[i] = '\0';
-
-    delete[] str.data;
-    str.length = i;
-    str.size = str.length + 1;
-    str.data = new char[str.size];
-    std::strcpy(str.data, buffer);
+    str = buffer;
 
     return is;
 }
@@ -52,12 +117,54 @@ std::ostream &operator<<(std::ostream &os, const MinString &str) {
     return os;
 }
 
-const char *MinString::getData() const {
-    return data;
+
+// Private methods
+void MinString::copy(const MinString &other) {
+    length = other.length;
+    size = other.size;
+    data = new char[size];
+    std::strcpy(data, other.data);
 }
 
-MinString::MinString(const MinString &other) {
-    length = other.length;
-    data = new char[length + 1];
-    strcpy(data, other.data);
+void MinString::copy(const char *_data) {
+    length = std::strlen(_data);
+    size = length + 1;
+    data = new char[size];
+    std::strcpy(data, _data);
 }
+
+
+void MinString::move(MinString &other) noexcept {
+    length = other.length;
+    size = other.size;
+    data = other.data;
+    other.length = 0;
+    other.size = 8;
+    other.data = new char[other.size];
+}
+
+void MinString::destroy() {
+    delete[] data;
+}
+
+void MinString::scaleUp(size_t minLength) {
+    if (minLength != 0 && size * 2 < minLength) {
+        size = minLength * 2;
+    } else {
+        size *= 2;
+    }
+    char *tmp = new char[size];
+    for (int i = 0; i < length; ++i) {
+        tmp[i] = data[i];
+    }
+    delete[]data;
+    data = tmp;
+}
+
+
+
+
+
+
+
+
--- Course2/SummerSemester/ReOOP/Homework02/MinString.h
+++ Course2/SummerSemester/ReOOP/Homework02/MinString.h
@@ -8,107 +8,51 @@
 
 class MinString {
 private:
-    char *data;
-    size_t length;
-    size_t size;
+    char *data{};
+    size_t length{};
+    size_t size{};
 
 public:
     MinString();
 
     MinString(const char *_data);
-    
+
+    ~MinString();
+
+    MinString(MinString &&other) noexcept;
+
+    MinString &operator=(MinString &&other) noexcept;
+
     MinString(const MinString &other);
 
-    // Move constructor
-    MinString(MinString &&other) noexcept: data(other.data), length(other.length) {
-        other.data = nullptr;
-        other.length = 0;
-    }
+    MinString &operator=(const char *_data);
 
-    // Destructor
-    ~MinString() {
-        delete[] data;
-    }
+    MinString &operator=(const MinString &other);
 
-    // Assignment operator for const char*
-    MinString &operator=(const char *data) {
-        delete[] data;
-        length = strlen(data);
-        data = new char[length + 1];
-        strcpy(data, data);
-        return *this;
-    }
+    size_t getLength() const;
 
-    // Assignment operator for integers
-    template<typename T>
-    MinString &operator=(T value) {
-        std::ostringstream oss;
-        oss << value;
-        return operator=(oss.str().c_str());
-    }
+    MinString &operator+=(const MinString &other);
 
-    // Copy assignment operator
-    MinString &operator=(const MinString &other) {
-        if (this != &other) {
-            delete[] data;
-            length = other.length;
-            data = new char[length + 1];
-            strcpy(data, other.data);
-        }
-        return *this;
-    }
-
-    // Move assignment operator
-    MinString &operator=(MinString &&other) noexcept {
-        if (this != &other) {
-            delete[] data;
-            data = other.data;
-            length = other.length;
-            other.data = nullptr;
-            other.length = 0;
-        }
-        return *this;
-    }
-
-    // Concatenation operator (+)
-    MinString operator+(const MinString &other) const {
-        MinString result;
-        result.length = length + other.length;
-        result.data = new char[result.length + 1];
-        strcpy(result.data, data);
-        strcat(result.data, other.data);
-        return result;
-    }
-
-    // Compound assignment operator (+=)
-    MinString &operator+=(const MinString &other) {
-        char *temp = new char[length + other.length + 1];
-        strcpy(temp, data);
-        strcat(temp, other.data);
-
-        delete[] data;
-        length += other.length;
-        data = temp;
-
-        return *this;
-    }
-
-    // Getter for the string length
-    int length() const {
-        return length;
-    }
-
-    const char *getData() const;
+    friend MinString &operator+(const MinString &lvs, const MinString &rvs);
 
     friend std::ostream &operator<<(std::ostream &os, const MinString &str);
 
     friend std::istream &operator>>(std::istream &is, MinString &str);
 
+    friend bool operator==(const MinString &lvs, const MinString &rvs);
 
-    friend bool operator==(const MinString &lhs, const MinString &rhs);
+    friend bool operator!=(const MinString &lvs, const MinString &rvs);
 
 private:
+    void copy(const MinString &other);
 
+    void copy(const char *_data);
+
+    void move(MinString &other) noexcept;
+
+    void scaleUp(size_t minLength = 0);
+
+    void destroy();
 };
 
 
