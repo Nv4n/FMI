cf558b2e52e1ab0b76cf08dc53a9c0d5c87dce9c
Sybatron <vipsister2014@gmail.com>
2024-11-22 01:27:24 +0200

    added distance

--- 2024-2025/SOZ-knowledge-systems/hw1/fn3mi0700035.py
+++ 2024-2025/SOZ-knowledge-systems/hw1/fn3mi0700035.py
@@ -1,7 +1,9 @@
 import sys
 from collections import deque
+import math
 
 from utils import *
+
 romania_map={}
 romania_map["locations"] = dict(
 Arad=(91, 492), Bucharest=(400, 327), Craiova=(253, 288),
@@ -11,3 +13,8 @@ Lugoj=(165, 379), Mehadia=(168, 339), Neamt=(406, 537),
 Oradea=(131, 571), Pitesti=(320, 368), Rimnicu=(233, 410),
 Sibiu=(207, 457), Timisoara=(94, 410), Urziceni=(456, 350),
 Vaslui=(509, 444), Zerind=(108, 531))
+
+def distance(begin, end) :
+    return math.hypot(begin[0]-end[0],begin[1]-end[1])
+    
+print(distance(romania_map["locations"].get("Arad"),romania_map["locations"].get("Zerind")))
