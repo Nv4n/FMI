4f30d6794396f83c6fba7a18c3ce824098252839
Sybatron <vipsister2014@gmail.com>
2024-12-05 17:09:15 +0200

    feat(xml-proj): added download()

--- 2024-2025/xml-technologies/agro-nursery-warehouse/src/index.js
+++ 2024-2025/xml-technologies/agro-nursery-warehouse/src/index.js
@@ -1,1 +1,8 @@
 //@ts-check
+import { download, serializeDocument } from "./utility";
+import { generateDoc } from "./xml-doc-generator";
+
+(function downloadGeneratedFile() {
+    let doc = generateDoc();
+    download("test.xml", serializeDocument(doc));
+});
--- 2024-2025/xml-technologies/agro-nursery-warehouse/src/utility.js
+++ 2024-2025/xml-technologies/agro-nursery-warehouse/src/utility.js
@@ -60,3 +60,40 @@ export function toLatin(cyrillic) {
         .concat(`${Math.floor(Math.random() * 2000)}`)
         .join("");
 }
+
+/**
+ *
+ * @param {XMLDocument} doc
+ * @returns {string}
+ */
+export function serializeDocument(doc) {
+    const serializer = new XMLSerializer();
+    const serializedDocument = serializer.serializeToString(doc);
+
+    return formatXml(serializedDocument, {
+        collapseContent: true,
+        throwOnFailure: false,
+    });
+}
+
+/**
+ *
+ * @param {string} filename
+ * @param {*} content
+ */
+export function download(filename, content) {
+    const file = new File([content], filename);
+    const url = URL.createObjectURL(file);
+
+    const tempLinkElement = document.createElement("a");
+    tempLinkElement.setAttribute("href", url);
+    tempLinkElement.setAttribute("download", filename);
+
+    tempLinkElement.style.display = "none";
+    document.body.appendChild(tempLinkElement);
+
+    tempLinkElement.click();
+
+    document.body.removeChild(tempLinkElement);
+    URL.revokeObjectURL(url);
+}
